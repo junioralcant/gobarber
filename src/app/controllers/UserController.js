ce2fe3b79cb1f3fcafb03fc0b8cdbdca45c0a06f
Junior Marques <junioralcant111@gmail.com>
2019-10-03 16:57:54 -0300

    Update usuário

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -21,6 +21,38 @@ class UserController {
       provider
     });
   }
+
+  async update(req, res) {
+    const { email, oldPassword } = req.body;
+
+    const user = await User.findByPk(req.userId); // busca o usuário pa PK
+
+    if (email != user.email) {
+      const userExists = await User.findOne({ where: { email } }); // verifica se o email informado já existe no bd
+
+      if (userExists) {
+        return res
+          .status(400)
+          .json({ error: "Endereço de e-mail não encontrado." });
+      }
+    }
+
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return res
+        .status(401)
+        .json({
+          error: "A senha informada não corresponde com a antiga senha"
+        });
+    }
+    const { id, name, provider } = await user.update(req.body);
+
+    return res.json({
+      id,
+      name,
+      email,
+      provider
+    });
+  }
 }
 
 module.exports = new UserController();
--- src/routes.js
+++ src/routes.js
@@ -3,9 +3,15 @@ const { Router } = require("express");
 const UserController = require("./app/controllers/UserController");
 const SessionController = require("./app/controllers/SessionController");
 
+const authMiddleware = require("./app/middleware/auth");
+
 const routes = new Router();
 
-routes.post("/users", UserController.store);
 routes.post("/sessions", SessionController.store);
+
+routes.use(authMiddleware);
+
+routes.post("/users", UserController.store);
+routes.put("/users", UserController.update);
 
 module.exports = routes;
