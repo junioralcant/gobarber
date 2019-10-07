bdc0699b12a45b16bb26f99059b6817fb21a9c88
Junior Marques <junioralcant111@gmail.com>
2019-10-07 15:35:11 -0300

    Conectando ao mongodb

--- src/databese/index.js
+++ src/databese/index.js
@@ -1,4 +1,5 @@
 const Sequileze = require("sequelize");
+const mongoose = require("mongoose");
 
 const User = require("../app/models/User");
 const File = require("../app/models/File");
@@ -11,6 +12,7 @@ const models = [User, File, Appointment];
 class Database {
   constructor() {
     this.init();
+    this.mongo();
   }
 
   init() {
@@ -19,6 +21,18 @@ class Database {
     models
       .map(model => model.init(this.connection))
       .map(model => model.associate && model.associate(this.connection.models)); // executa o metodo associate se o mesmo existir em algum model
+  }
+
+  // conexão com mongodb
+  mongo() {
+    this.mongoConnection = mongoose.connect(
+      "mongodb://localhost:27017/gobarber",
+      {
+        useNewUrlParser: true,
+        useFindAndModify: true,
+        useUnifiedTopology: true
+      }
+    );
   }
 }
 
