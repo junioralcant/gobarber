f7112f168b74f819f80380eb3983416c2da09375
Junior Marques <junioralcant111@gmail.com>
2019-10-11 11:25:27 -0300

    Tratamento de exceções com sentry e youch

--- src/app.js
+++ src/app.js
@@ -1,5 +1,10 @@
 const express = require("express");
 const path = require("path");
+const Sentry = require("@sentry/node");
+const Youch = require("youch");
+require("express-async-errors"); // permite que o express capti erros async
+
+const sentryConfig = require("./config/sentry");
 const routes = require("./routes");
 
 require("./databese"); // importa a conexão com bd,
@@ -8,11 +13,15 @@ class App {
   constructor() {
     this.server = express();
 
+    Sentry.init(sentryConfig);
+
     this.middlewares();
     this.routes();
+    this.exceptionHandler();
   }
 
   middlewares() {
+    this.server.use(Sentry.Handlers.requestHandler());
     this.server.use(express.json()); // informa que ira retornar dados em json
     // faz com que a rota /files tenha acesso a pasta uploads sem autenticação
     this.server.use(
@@ -23,6 +32,15 @@ class App {
 
   routes() {
     this.server.use(routes); // importa as rotas
+    this.server.use(Sentry.Handlers.errorHandler());
+  }
+
+  exceptionHandler() {
+    this.server.use(async (err, req, res, next) => {
+      const errors = await new Youch(err, req).toJSON();
+
+      return res.status(500).json(errors);
+    });
   }
 }
 
---
+++ src/config/sentry.js
@@ -0,0 +1,3 @@
+module.exports = {
+  dsn: "https://f1733bcb73e1436a95727b61908bc0f9@sentry.io/1776860"
+};
